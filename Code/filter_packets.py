bef1a4c9920badceafef2ed64c2592cb6f15c878
GavinHunsinger <89426382+GavinHunsinger@users.noreply.github.com>
2022-11-17 15:22:11 -0500

    getting one packet but have to get next
    I have to use in operator rather than splitting. The file ends when the line is blank or that's when I know next packet will start

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,32 +1,35 @@
 #filename is the name of the file being filtered
 #data is a list that all ICMP echo requests and replies will be stored in
 
-#import regex
+#look "in" on line and do not split
+#skip blank lines and know that file will end with blank line
+import re
 #just splitting and .join
 def filter(filename, data) :
 	#print('called filter function in filter_packets.py')
 	infile = open(filename, 'r')
-	line = infile.readline().strip().split(" ")
-
+	line = re.sub(' +', ' ', infile.readline())
+	line = line.split(" ")
 	#used to group the full packet together in order to put it into the data list
 	temp_storage = []
-
 	while line:
-		#print(line[0])
 		if(line[0] == "No."):
-			#print(line)
-			line = infile.readline().strip().split(" ")
-			#print("This is 4: " + line[26]) indices 26 and 37 because of white space but it should be 4 and 6 otherwise
-			#print(line)
-			if(line[26] == "ICMP" and line[36] == "Echo"):
+			line = line = re.sub(' +', ' ', infile.readline())
+			line = line.split(" ")
+			if(line[5] == "ICMP" and line[7] == "Echo"):
 				#start adding to temp_storage
 				temp_storage.append(line)
-				for i in range(4):
-					line = infile.readline().strip().split(" ")
+				for i in range(6):
+					line = re.sub(' +', ' ', infile.readline())
+					line = line.split(" ")
 					temp_storage.append(line)
-			data.append(temp_storage)	
-		elif(line[0] != "No."):
-			line = infile.readline()
+				data.append(temp_storage)
+				line = re.sub(' +', ' ', infile.readline())
+				line = line.split(" ")	
+		#WILL NOT GO TO THE NEXT PACKET
+			elif not(line[5] == "ICMP") and not (line[7] == "Echo"):
+				line = re.sub(' +', ' ', infile.readline())
+				print(line)
 	infile.close()
 
 
@@ -37,7 +40,4 @@ filter(filename, data)
 
 #print(data)
 #debugging purposes
-for i in range(len(data)):
-	print(data[i])
-	print("\n")
-	
+print(data)
