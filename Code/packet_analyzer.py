2061fa17d0a5cd8ef342f53e3522c2b9fae4aec0
GavinHunsinger <89426382+GavinHunsinger@users.noreply.github.com>
2022-11-29 14:45:40 -0500

    Updated to filter all nodes
    Updated to filter all nodes

--- Code/packet_analyzer.py
+++ Code/packet_analyzer.py
@@ -2,6 +2,12 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-filter()
+#filters nodes 1-4
+filter('Node1.txt')
+filter('Node2.txt')
+filter('Node3.txt')
+filter('Node4.txt')
+
+
 parse()
 compute()
